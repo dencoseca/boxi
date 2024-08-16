05ef8edf2074ff8a060d2d152ce2b58b4ee5028c
dencoseca <leonbrown@Leons-MacBook-Pro-2.local>
2024-08-16 17:26:37 +0100

    Add pluralization function and improve error handling
    Refactored container and volume handling functions to use a new pluralise function for cleaner pluralization. Switched from printMessage to log.Fatal for error reporting, and streamlined condition checks. Removed redundant system prune function and adjusted main usage message handling.

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"os/exec"
 	"strings"
@@ -14,6 +15,15 @@ const (
 	Success MessageType = "success"
 	Warning MessageType = "warning"
 )
+
+func pluralise(count int) string {
+	return func(n int) string {
+		if n == 1 {
+			return ""
+		}
+		return "s"
+	}(count)
+}
 
 func printMessage(message string, msgType ...MessageType) {
 	reset := "\x1B[0m"
@@ -46,129 +56,141 @@ func runCommand(command string, args ...string) ([]byte, error) {
 func stopContainers() {
 	output, err := runCommand("docker", "ps", "-a", "--format", "{{.Names}}")
 	if err != nil {
-		printMessage("Failed to list containers", Danger)
+		log.Fatal(err)
+	}
+
+	containerNames := strings.Fields(string(output))
+	if len(containerNames) <= 0 {
+		printMessage("No CONTAINERS to STOP", Danger)
 		return
 	}
-	containerNames := strings.Fields(string(output))
-	if len(containerNames) > 0 {
-		printMessage("STOPPING CONTAINERS", Success)
-		stoppedContainerCount := 0
-		for _, container := range containerNames {
-			_, err = runCommand("docker", "stop", container)
-			if err != nil {
-				printMessage(fmt.Sprintf("Failed to stop container %s: %v", container, err), Danger)
-			} else {
-				stoppedContainerCount++
-			}
+
+	printMessage("STOPPING CONTAINERS", Success)
+	stoppedContainerCount := 0
+	for _, container := range containerNames {
+		_, err = runCommand("docker", "stop", container)
+		if err != nil {
+			printMessage(fmt.Sprintf("Failed to stop container %s: %v", container, err), Danger)
+		} else {
+			stoppedContainerCount++
 		}
-		message := fmt.Sprintf("Stopped %d container%s", stoppedContainerCount, func(n int) string {
-			if n == 1 {
-				return ""
-			}
-			return "s"
-		}(stoppedContainerCount))
-		printMessage(message)
-	} else {
-		printMessage("No CONTAINERS to STOP", Danger)
 	}
+	message := fmt.Sprintf("Stopped %d container%s", stoppedContainerCount, pluralise(stoppedContainerCount))
+	printMessage(message)
 }
 
 func removeContainers() {
 	output, err := runCommand("docker", "ps", "-a", "--format", "{{.Names}}")
 	if err != nil {
-		printMessage("Failed to list containers", Danger)
+		log.Fatal(err)
+	}
+
+	containerNames := strings.Fields(string(output))
+	if len(containerNames) <= 0 {
+		printMessage("No CONTAINERS to REMOVE", Danger)
 		return
 	}
-	containerNames := strings.Fields(string(output))
-	if len(containerNames) > 0 {
-		printMessage("REMOVING CONTAINERS", Success)
-		removedContainerCount := 0
-		for _, container := range containerNames {
-			_, err = runCommand("docker", "rm", container)
-			if err != nil {
-				printMessage(fmt.Sprintf("Failed to remove container %s: %v", container, err), Danger)
-			} else {
-				removedContainerCount++
-			}
+
+	printMessage("REMOVING CONTAINERS", Success)
+	removedContainerCount := 0
+	for _, container := range containerNames {
+		_, err = runCommand("docker", "rm", container)
+		if err != nil {
+			printMessage(fmt.Sprintf("Failed to remove container %s: %v", container, err), Danger)
+		} else {
+			removedContainerCount++
 		}
-		message := fmt.Sprintf("Removed %d container%s", removedContainerCount, func(n int) string {
-			if n == 1 {
-				return ""
-			}
-			return "s"
-		}(removedContainerCount))
-		printMessage(message)
-	} else {
-		printMessage("No CONTAINERS to REMOVE", Danger)
 	}
+	message := fmt.Sprintf("Removed %d container%s", removedContainerCount, pluralise(removedContainerCount))
+	printMessage(message)
 }
 
 func removeVolumes() {
 	output, err := runCommand("docker", "volume", "ls", "-q")
 	if err != nil {
-		printMessage("Failed to list volumes", Danger)
+		log.Fatal(err)
+	}
+
+	volumeNames := strings.Fields(string(output))
+	if len(volumeNames) <= 0 {
+		printMessage("No VOLUMES to REMOVE", Danger)
 		return
 	}
-	volumeNames := strings.Fields(string(output))
-	if len(volumeNames) > 0 {
-		printMessage("REMOVING VOLUMES", Success)
-		removedVolumeCount := 0
-		for _, volume := range volumeNames {
-			_, err = runCommand("docker", "volume", "rm", volume)
-			if err != nil {
-				printMessage(fmt.Sprintf("Failed to remove volume %s: %v", volume, err), Danger)
-			} else {
-				removedVolumeCount++
-			}
+
+	printMessage("REMOVING VOLUMES", Success)
+	removedVolumeCount := 0
+	for _, volume := range volumeNames {
+		_, err = runCommand("docker", "volume", "rm", volume)
+		if err != nil {
+			printMessage(fmt.Sprintf("Failed to remove volume %s: %v", volume, err), Danger)
+		} else {
+			removedVolumeCount++
 		}
-		message := fmt.Sprintf("Removed %d volume%s", removedVolumeCount, func(n int) string {
-			if n == 1 {
-				return ""
-			}
-			return "s"
-		}(removedVolumeCount))
-		printMessage(message)
-	} else {
-		printMessage("No VOLUMES to REMOVE", Danger)
 	}
+	message := fmt.Sprintf("Removed %d volume%s", removedVolumeCount, pluralise(removedVolumeCount))
+	printMessage(message)
 }
 
 func removeImages() {
 	output, err := runCommand("docker", "images", "-q")
 	if err != nil {
-		printMessage("Failed to list images", Danger)
+		log.Fatal(err)
+	}
+
+	imageIDs := strings.Fields(string(output))
+	if len(imageIDs) <= 0 {
+		printMessage("No IMAGES to REMOVE", Danger)
 		return
 	}
-	imageIDs := strings.Fields(string(output))
-	if len(imageIDs) > 0 {
-		printMessage("REMOVING IMAGES", Success)
-		removedImageCount := 0
-		for _, image := range imageIDs {
-			_, err = runCommand("docker", "rmi", image)
-			if err != nil {
-				printMessage(fmt.Sprintf("Failed to remove image %s: %v", image, err), Danger)
-			} else {
-				removedImageCount++
-			}
+
+	printMessage("REMOVING IMAGES", Success)
+	removedImageCount := 0
+	for _, image := range imageIDs {
+		_, err = runCommand("docker", "rmi", image)
+		if err != nil {
+			printMessage(fmt.Sprintf("Failed to remove image %s: %v", image, err), Danger)
+		} else {
+			removedImageCount++
 		}
-		message := fmt.Sprintf("Stopped %d image%s", removedImageCount, func(n int) string {
-			if n == 1 {
-				return ""
-			}
-			return "s"
-		}(removedImageCount))
-		printMessage(message)
+	}
+	message := fmt.Sprintf("Stopped %d image%s", removedImageCount, pluralise(removedImageCount))
+	printMessage(message)
+}
+
+func pruneSystem() {
+	output, err := runCommand("docker", "system", "prune", "-f")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	lines := strings.Split(string(output), "\n")
+	var reclaimedSpace string
+
+	for _, line := range lines {
+		if strings.Contains(line, "Total reclaimed space") {
+			reclaimedSpace = line
+			break
+		}
+	}
+
+	if reclaimedSpace != "Total reclaimed space: 0B" {
+		printMessage("Pruning SYSTEM", Success)
+		printMessage(reclaimedSpace)
 	} else {
-		printMessage("No IMAGES to REMOVE", Danger)
+		printMessage("NOTHING to PRUNE", Danger)
 	}
 }
 
 func main() {
+	usage := "Usage: boxi <command>"
+
 	if len(os.Args) < 2 {
-		fmt.Println("Usage: boxi <command>")
+		fmt.Println(usage)
 		return
 	}
+
 	command := os.Args[1]
+
 	switch command {
 	case "sc":
 		stopContainers()
@@ -192,29 +214,6 @@ func main() {
 		removeImages()
 		pruneSystem()
 	default:
-		fmt.Println("Unknown command:", command)
-	}
-}
-
-func pruneSystem() {
-	output, err := runCommand("docker", "system", "prune", "-f")
-	if err != nil {
-		printMessage("Pruning SYSTEM", Success)
-		printMessage("Failed to prune system", Danger)
-	} else {
-		lines := strings.Split(string(output), "\n")
-		var reclaimedSpace string
-		for _, line := range lines {
-			if strings.Contains(line, "Total reclaimed space") {
-				reclaimedSpace = line
-				break
-			}
-		}
-		if reclaimedSpace != "Total reclaimed space: 0B" {
-			printMessage("Pruning SYSTEM", Success)
-			printMessage(reclaimedSpace)
-		} else {
-			printMessage("NOTHING to PRUNE", Danger)
-		}
+		fmt.Println(usage)
 	}
 }
