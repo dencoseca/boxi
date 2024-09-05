de1fbedf4252ce3dcca79a55e894af62588f2108
dencoseca <leonbrown@Leons-MacBook-Pro-2.local>
2024-09-05 16:36:33 +0100

    Change package name comment to 'boxi'
    Update the package name in the comment header to 'boxi' to reflect the project's current branding. This ensures consistency with the project's naming conventions.

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-// Package main provides a command-line tool for managing Docker containers,
+// boxi provides a command-line tool for managing Docker containers,
 // volumes, and images. The tool supports operations such as stopping, removing,
 // and cleaning containers, removing volumes and images, and performing system
 // cleanup actions like wiping and purging.
