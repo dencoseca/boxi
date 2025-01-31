9880d2f1e1ece4de1f06b4d28f72d2eed2c28b1c
dencoseca <leonbrown@Leons-MacBook-Pro-2.local>
2025-01-31 20:23:01 +0000

    "Remove unused colorise function and MessageType constants
    Eliminated the redundant colorise function and MessageType constants to clean up the utils.go file. This simplifies the codebase and removes unused features that are no longer relevant."

--- utils.go
+++ utils.go
@@ -1,42 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"os/exec"
 )
-
-// MessageType represents the type or category of a message in the system.
-type MessageType int
-
-const (
-	Danger MessageType = iota
-	Success
-	Warning
-)
-
-// colorise applies a color style to a given message string based on the provided
-// MessageType. If no MessageType is provided, it uses the default style.
-func colorise(message string, msgType ...MessageType) string {
-	reset := "\x1B[0m"
-	var style string
-
-	if len(msgType) > 0 {
-		switch msgType[0] {
-		case Danger:
-			style = "\x1B[1;31m"
-		case Success:
-			style = "\x1B[1;32m"
-		case Warning:
-			style = "\x1B[1;33m"
-		default:
-			style = reset
-		}
-	} else {
-		style = reset
-	}
-
-	return fmt.Sprintf("%s%s%s", style, message, reset)
-}
 
 // pluralise returns the string "s" if the count is not equal to 1, otherwise an
 // empty string.
